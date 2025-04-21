51ef3d87e48199f5d8e731113d5f7f33c7be89c5
quartzs2 <csh001231@gmail.com>
2025-04-21 15:38:02 +0900

    chore: App.jsx에서 내려주던 props 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,15 @@ import Header from "./components/Header";
 import Menu from "./components/Menu";
 import { Route, Routes } from "react-router-dom";
 import Cart from "./components/Cart";
-import { useMenu } from "./hooks/useMenu";
-import { useCart } from "./hooks/useCart";
 
 function App() {
-  const { menu } = useMenu();
-  const { cart, setCart } = useCart();
-  console.log(cart);
-
   return (
     <div>
       <Header />
       <main>
         <Routes>
-          <Route path="/" element={<Menu menu={menu} cart={cart} setCart={setCart} />} />
-          <Route path="/cart" element={<Cart menu={menu} cart={cart} setCart={setCart} />} />
+          <Route path="/" element={<Menu />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </main>
     </div>
